099d1971eecd3441ebdc097df4403b26611b50fd
HussainOsama <hussain.osama.asd@gmail.com>
2021-02-01 19:16:23 +0300

    Done the task.

---
+++ story.js
@@ -0,0 +1,8 @@
+let name = "Hussain";
+let age = "25";
+let hoppy = "Cycling"
+
+let print = `Mr.${name}, in ${age} years, you will be ${age+33} years old. By then you'll have decades of ${hoppy} practice!`
+
+console.log(print);
+
